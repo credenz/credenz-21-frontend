416deb458049e4f619c40289867aeee88902545a
Pranav Mohril <pranavmohril@gmail.com>
2021-12-19 14:27:24 +0530

    events cards responsive fix

--- src/Components/EventCard1.js
+++ src/Components/EventCard1.js
@@ -1,32 +1,33 @@
 import React from "react";
 import "../CSS/eventcard1.css";
+import { Container } from "react-bootstrap";
 const EventCard1 = (props) => {
   const [hovered, setHovered] = React.useState(false);
   return (
-    <div style={{ width: props.width || "270px" }}>
-      <div className="wrapper">
-        <div
-          onMouseEnter={() => {
-            setHovered(true);
-          }}
-          onMouseLeave={() => {
-            setHovered(false);
-          }}
-          className="hover-effect-1"
-          style={{
-            backgroundColor: "#000",
-            height: props.height || "350px",
-            width: props.width || "270px",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div className={`hover-card-text ${hovered ? "hovered" : ""}`}>
-          <span>{props.title}</span>
-        </div>
+    // <Container style={{ width: props.width || "270px" }}>
+    <div className="wrapper">
+      <div
+        onMouseEnter={() => {
+          setHovered(true);
+        }}
+        onMouseLeave={() => {
+          setHovered(false);
+        }}
+        className="hover-effect-1"
+        style={{
+          backgroundColor: "#000",
+          height: props.height || "350px",
+          width: props.width || "270px",
+          // backgroundSize: "cover",
+          // backgroundPosition: "center",
+          // backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+      <div className={`hover-card-text ${hovered ? "hovered" : ""}`}>
+        <span>{props.title}</span>
       </div>
     </div>
+    // </Container>
   );
 };
 
--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -3,18 +3,50 @@ import EventCard1 from "../Components/EventCard1";
 import "../CSS/events.css";
 const Events = () => {
   return (
-    <div className="container bg-dark height-full">
-      <div className="row justify-content-around d-flex mb-3">
-        <EventCard1 width="180px" height="220px" title="Clash" />
-        <EventCard1 width="180px" height="220px" title="RC" />
-        <EventCard1 width="180px" height="220px" title="NTH" />
-        <EventCard1 width="180px" height="220px" title="WallStreet" />
-      </div>
-      <div className="row justify-content-around d-flex mb-3">
-        <EventCard1 width="180px" height="220px" title="Datawiz" />
-        <EventCard1 width="180px" height="220px" title="Quiz" />
-        <EventCard1 width="180px" height="220px" title="Presentation" />
-        <EventCard1 width="180px" height="220px" title="Cretronix" />
+    <div className="bg-dark section-events height-full o-x-hide">
+      <div className="container">
+        <div className="row justify-content-around d-flex mb-3 gx-5">
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="Clash" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="RC" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="NTH" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="WallStreet" />
+          </div>
+        </div>
+        <div className="row justify-content-around d-flex mb-3 gx-5">
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="B-Plan" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="Vertigo" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="Datawiz" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="Quiz" />
+          </div>
+        </div>
+        <div className="row justify-content-around d-flex mb-3 gx-5">
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="Presentation" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="Cretronix" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="5George" />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard1 width="150px" height="180px" title="Contrary" />
+          </div>
+        </div>
       </div>
     </div>
   );
