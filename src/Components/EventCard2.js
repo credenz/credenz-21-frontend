6263b34f739608efad44ae9e47082f4d31a19c03
Pranav Mohril <pranavmohril@gmail.com>
2022-01-20 13:19:19 +0530

    events cards finalized

--- src/Components/EventCard2.js
+++ src/Components/EventCard2.js
@@ -3,15 +3,16 @@ import "../CSS/eventcard2.css";
 
 const EventCard2 = (props) => {
   return (
-    <div class="wrapper-new">
-      <div class="container-new">
-        <div class="top">
+    <div className="wrapper-new">
+      <div className="container-new">
+        <div className="top">
           {props.icon && (
             <img
               src={props.icon}
               alt="Icon"
               height="120px"
               width="120px"
+              className="event-icon"
               style={{
                 color: "white",
                 marginLeft: "20px",
@@ -20,19 +21,22 @@ const EventCard2 = (props) => {
             />
           )}
         </div>
-        <div class="bottom">
-          <div class="left">
-            <div class="details">
-              <h1>Reverse Coding</h1>
+        <div className="bottom">
+          <div className="left">
+            <div className="details">
+              <h1>{props.title}</h1>
             </div>
           </div>
         </div>
       </div>
-      <div class="inside">
-        <div class="icon">*</div>
-        <div class="contents">
-          Hone your problem-solving skills by decrypting complex questions
+      <div className="inside">
+        <div className="icon">
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAABf0lEQVRIidWVTU4CQRCFqyGwEW9gPAC60gOIC13qYIxXIBDjzyXIxDPoHeQaRheiC0VdafQAbAQjnwuqpSPd8yMrKul0Zt7rejVvqrtF5j1MEgiURSTSsSYiSwq9iciNiHRE5NIYM8ytDOwBL6THM1DPk7gAnDkJboEjoAos6FgBjoGuw4uBQhYBm/wTaCQt0mKaygWIs9hik2/k+OqaIxKFSGXH80aAA0AAayn8BJR8hAPHc68tKQJF4E4p+/a9m8h2wrkxZuRLYjQC2LeIXOjjtE3Ao6pXfQmyhHYXwIMP7CtYSUgQtEjxRaX07bv0vs0X1r5fi12Bd52XZxCwaz98Atc6b80gsK3z1RTitGl3hja9/9umLqHM+OACaP5D4FDhnnejKanuHBU1L8m/bhMYACNgJ40cOyItoJjALWrlA13TzlJNwRFBt/+JbqKKjlXg1PF8BLRD/y4kFDE+uNKil2RL2pVZEpFdGZ8t6zK5Ml9lcmV2jDFfmSufu/gBIo4rqpw825AAAAAASUVORK5CYII="
+            alt="IIcon"
+          />
         </div>
+        {props.text && <div className="contents">{props.text}</div>}
       </div>
     </div>
   );
--- src/Pages/Events.js
+++ src/Pages/Events.js
@@ -3,16 +3,28 @@ import EventCard1 from "../Components/EventCard1";
 import EventCard2 from "../Components/EventCard2";
 import "../CSS/events.css";
 import RC from "../images/rc.png";
+import Clash from "../images/clash.png";
+import BPlan from "../images/bplan.png";
+import Cretronix from "../images/cretronix.png";
+import Datawiz from "../images/datawiz.png";
+import Enigma from "../images/enigma.png";
+import Paper from "../images/paper.png";
+import Pixelate from "../images/pixelate.png";
+import Quiz from "../images/quiz.png";
+import Wallstreet from "../images/wallstreet.png";
+import Web from "../images/web.png";
+import NTH from "../images/nth.png";
+
 const Events = () => {
   const height = "250px";
   const width = "190px";
   return (
     <div className="bg-dark section-events height-full o-x-hide">
       <div className="container">
-        <EventCard2 icon={RC} />
         <div className="row justify-content-start d-flex mb-3 gx-5">
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1
+            <EventCard2
+              icon={RC}
               width={width}
               height={height}
               title="Reverse Coding"
@@ -20,46 +32,107 @@ const Events = () => {
             />
           </div>
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1
+            <EventCard2
+              icon={Clash}
               width={width}
               height={height}
               title="Clash"
-              text="Hone your problem-solving skills by decrypting complex questions"
+              text="Clash is based on Competitive Programming!"
             />
           </div>
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="NTH" />
+            <EventCard2
+              icon={NTH}
+              width={width}
+              height={height}
+              title="NTH"
+              text="Decrypt the clues to solve complex puzzles and race your way to finish at top!"
+            />
           </div>
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="WallStreet" />
-          </div>
-        </div>
-        <div className="row justify-content-start d-flex mb-3 gx-5">
-          <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="B-Plan" />
-          </div>
-          <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="Vertigo" />
-          </div>
-          <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="Datawiz" />
-          </div>
-          <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="Quiz" />
+            <EventCard2
+              icon={Wallstreet}
+              width={width}
+              height={height}
+              title="WallStreet"
+              text="Get an insight on stock markets by Investing and trading virtual money!"
+            />
           </div>
         </div>
         <div className="row justify-content-start d-flex mb-3 gx-5">
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="Presentation" />
+            <EventCard2
+              icon={BPlan}
+              width={width}
+              height={height}
+              title="B-Plan"
+              text="Showcase your entrepreneurial skills by presenting innovative business plans to the world!"
+            />
           </div>
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="Cretronix" />
+            <EventCard2
+              icon={Enigma}
+              width={width}
+              height={height}
+              title="Enigma"
+              text="Get ready to push your aptitude, logical reasoning skills limits by solving mind-boggling questions!"
+            />
           </div>
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="5George" />
+            <EventCard2
+              icon={Datawiz}
+              width={width}
+              height={height}
+              title="Datawiz"
+              text="Test your Machine learning and data analytics skills before stepping into the world of Data Science!"
+            />
           </div>
           <div className="col-md-3 justify-content-center m-mb">
-            <EventCard1 width={width} height={height} title="Contrary" />
+            <EventCard2
+              icon={Quiz}
+              width={width}
+              height={height}
+              title="Quiz"
+              text="Test your knowledge across wide range of topics and get a chance to win some exciting prizes!"
+            />
+          </div>
+        </div>
+        <div className="row justify-content-start d-flex mb-3 gx-5">
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard2
+              icon={Paper}
+              width={width}
+              height={height}
+              title="Paper Presentation"
+              text="Present technical research papers with advanced perspectives while honing your skills of research!"
+            />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard2
+              icon={Cretronix}
+              width={width}
+              height={height}
+              title="Cretronix"
+              text="Build and exhibit your own circuits by solving tech problems!"
+            />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard2
+              icon={Pixelate}
+              width={width}
+              height={height}
+              title="Pixelate"
+              text="Let your creativity fuse with thoughtful designs and artistic originality!"
+            />
+          </div>
+          <div className="col-md-3 justify-content-center m-mb">
+            <EventCard2
+              icon={Web}
+              width={width}
+              height={height}
+              title="Webweaver"
+              text="Build an aesthetic website to showcase your web development skills!"
+            />
           </div>
         </div>
       </div>
