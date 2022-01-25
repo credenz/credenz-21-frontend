a43e4e4641759faa7ba28af28b94fac964be1792
Aniket <aniketfand471@gmail.com>
2022-01-25 22:12:00 +0530

    Modified about page

--- src/Components/AboutCards.js
+++ src/Components/AboutCards.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
 import '../CSS/AboutCards.css'
 
-function AboutCards({ m1, m2, m3, imgurl, eventname }) {
+function AboutCards({ m2, m3, imgurl, eventname, readm, readl }) {
 
-    const [massege, setMassege] = useState(m1)
-    const [flag, setFlag] = useState(false)
+    const [massege, setMassege] = useState(m2)
+    const [read, setRead] = useState(readm)
+    const [flag, setFlag] = useState(true)
 
     return (
         <div className='aboutCard' >
             <img src={imgurl} alt='Branchlogo'></img>
             <h2>{eventname}</h2>
             {flag ? (
-                <div className='aboutCard-massege' onClick={() => { setMassege(m3); setFlag(false) }} >{massege}</div>
+                <div className='aboutCard-messege' onClick={() => { setMassege(m3); setFlag(false); setRead(readl) }} >{massege}<br></br><strong>{read}</strong></div>
             ) : (
-                <div className='aboutCard-massege' onClick={() => { setMassege(m2); setFlag(true) }} >{massege}</div>
+                <div className='aboutCard-messege' onClick={() => { setMassege(m2); setFlag(true); setRead(readm); }} >{massege}<br></br><strong>{read}</strong></div>
             )}
 
         </div>
--- src/Components/AboutSponsors.js
+++ src/Components/AboutSponsors.js
@@ -5,6 +5,7 @@ function AboutSponsors() {
     return (
         <div className='aboutSponsor'>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKV3hJrxSfK8zRefbvWHYF-Zqiom0k0hXigw&usqp=CAU' alt='SponsorLogo'></img>
+            <h4>sponsorname</h4>
         </div>
     )
 }
