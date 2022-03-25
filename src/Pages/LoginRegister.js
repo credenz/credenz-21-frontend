21cdcf514398700f4f740dc39c3e30a84a76dd1d
Abhishek Dhar <abhishekdhar1603@gmail.com>
2022-03-25 12:36:53 +0530

    changed background

--- src/Pages/LoginRegister.js
+++ src/Pages/LoginRegister.js
@@ -10,34 +10,17 @@ const AppContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
-  z-index: 2;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 20px 0;
-  /* position: relative;
-  display: inline-block;
-  width: 100%; */
+ 
   vertical-align: middle;
   background-repeat: no-repeat;
   z-index: 101;
   background-attachment: fixed;
   background-size: cover;
-  height: 100vh;
   background-image: url("img/website bg.png");
   background-attachment: fixed;
   background-repeat: no-repeat;
   background-size: cover;
-  /* background-color: #111; */
-  /* background-image: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0.5)
-    ),
-    url("../images/hex.gif"); */
+ 
   background-position: center 0px;
 
   ${
