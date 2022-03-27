09a5ccbee821d1c82f6a0a8806a436090370d427
tanuj-22 <56783833+tanuj-22@users.noreply.github.com>
2022-03-27 22:23:44 +0530

    logo added with animation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,6 +10,7 @@ import CyberBg from "../vid/future_2.webm";
 import Loader from "../Components/Loader";
 import NavbarCustom from "../Components/NavbarCustom";
 import { Footer } from "../Components/Footer";
+import Animate from "../Components/Animation/Animate";
 
 const Home = () => {
   const [logoVisible, setLogoVisible] = useState(false);
@@ -61,7 +62,8 @@ const Home = () => {
                 className={
                   logoVisible ? "fade-in-delayed credenz-logo" : "d-none"
                 }>
-                <img src={HomeImage} alt="credenzLive" className="logo-img" />
+                {/* <img src={HomeImage} alt="credenzLive" className="logo-img" /> */}
+                <Animate/>
               </div>
             </div>
           </div>
